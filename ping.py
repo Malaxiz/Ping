ae24c5c5edc8bcb86ef5845241af8795dc751c6b
Malaxiz <malaxiz.main@gmail.com>
2016-01-16 10:08:47 +0100

    Removed ip list requirement from ping function

--- ping.py
+++ ping.py
@@ -8,16 +8,30 @@ import socket
 def generate_ips(range_start, range_end):
     return {"192.168.1."+str(ip): False for ip in range(range_start, range_end)}
 
+# credit: https://stackoverflow.com/users/416467/kindall
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs, Verbose)
+        self._return = None
+    def run(self):
+        if self._Thread__target is not None:
+            self._return = self._Thread__target(*self._Thread__args,
+                                                **self._Thread__kwargs)
+    def join(self):
+        Thread.join(self)
+        return self._return
 
-def ping(ip, ips):
+def ping(ip):
     call = subprocess.Popen(
         ["ping", "-c", "1", ip],
         stdout = subprocess.PIPE,
         stderr = subprocess.PIPE
     )
     out, error = call.communicate()
-    if not '0 packets received,' in out:
-        ips[ip] = True
+    if not '0 packets received,' in out and error == '':
+        return True
+    return False
 
 
 def print_nice(to_print):
@@ -31,13 +45,13 @@ def print_nice(to_print):
 
 
 def start(ips):
-    threads = []
+    threads = {}
     for ip in ips:
-        thread = Thread(target=ping, args=(ip, ips))
+        thread = ThreadWithReturnValue(target=ping, args=(ip,))
         thread.start()
-        threads.append(thread)
+        threads[ip] = thread
     for thread in threads:
-        thread.join()
+        ips[thread] = threads[thread].join()
     return {socket.gethostbyaddr(ip)[0]:ip for ip in ips if ips[ip] == True}
 
 #if __name__ == '__main__':
